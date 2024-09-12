09f59e51933483bf6566fd56bd9849c2ccf72fda
wisemoon <willyedoo@gmail.com>
2024-09-12 15:17:59 +0200

    Update CIForms_submissions.sql

--- postgres/CIForms_submissions.sql
+++ postgres/CIForms_submissions.sql
@@ -1,14 +1,14 @@
 --
--- Table structure for table `CIForms_submissions`
+-- Table structure for table CIForms_submissions
 --
 
-CREATE TABLE IF NOT EXISTS `ciforms_submissions` (
-  `id` int(11) NOT NULL,
-  `page_id` int(11) NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `data` blob NOT NULL,
-  `shown` datetime DEFAULT NULL,
-  `created_at` datetime NOT NULL
+CREATE TABLE IF NOT EXISTS ciforms_submissions (
+  id int(11) NOT NULL,
+  page_id int(11) NOT NULL,
+  title varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  data blob NOT NULL,
+  shown datetime DEFAULT NULL,
+  created_at datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -16,19 +16,19 @@ CREATE TABLE IF NOT EXISTS `ciforms_submissions` (
 --
 
 --
--- Indexes for table `CIForms_submissions`
+-- Indexes for table CIForms_submissions
 --
-ALTER TABLE `ciforms_submissions`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE ciforms_submissions
+  ADD PRIMARY KEY (id);
 
-ALTER TABLE `ciforms_submissions` ADD `username` VARCHAR(255) NULL AFTER `page_id`;
+ALTER TABLE ciforms_submissions ADD username VARCHAR(255) NULL AFTER page_id;
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `CIForms_submissions`
+-- AUTO_INCREMENT for table CIForms_submissions
 --
-ALTER TABLE `ciforms_submissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE ciforms_submissions
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
