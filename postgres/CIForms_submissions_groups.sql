313e3f4d981de9d44785df735d6a22e95ccce1e6
wisemoon <willyedoo@gmail.com>
2024-09-12 15:14:01 +0200

    Update CIForms_submissions_groups.sql

--- postgres/CIForms_submissions_groups.sql
+++ postgres/CIForms_submissions_groups.sql
@@ -1,12 +1,12 @@
 --
--- Table structure for table `CIForms_submissions_groups`
+-- Table structure for table CIForms_submissions_groups
 --
 
-CREATE TABLE IF NOT EXISTS `CIForms_submissions_groups` (
-  `id` int(11) NOT NULL,
-  `submission_id` int(11) NOT NULL,
-  `usergroup` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL
+CREATE TABLE IF NOT EXISTS CIForms_submissions_groups (
+  id int(11) NOT NULL,
+  submission_id int(11) NOT NULL,
+  usergroup varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  created_at datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -14,17 +14,17 @@ CREATE TABLE IF NOT EXISTS `CIForms_submissions_groups` (
 --
 
 --
--- Indexes for table `CIForms_submissions_groups`
+-- Indexes for table CIForms_submissions_groups
 --
-ALTER TABLE `CIForms_submissions_groups`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE CIForms_submissions_groups
+  ADD PRIMARY KEY (id);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `CIForms_submissions_groups`
+-- AUTO_INCREMENT for table CIForms_submissions_groups
 --
-ALTER TABLE `CIForms_submissions_groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE CIForms_submissions_groups
+  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
