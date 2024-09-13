988e4891fd045f85f880ddc3334ef519ff851bb9
wisemoon <willyedoo@gmail.com>
2024-09-13 13:36:22 +0200

    Update CIForms_submissions.sql

--- postgres/CIForms_submissions.sql
+++ postgres/CIForms_submissions.sql
@@ -3,32 +3,15 @@
 --
 
 CREATE TABLE IF NOT EXISTS ciforms_submissions (
-  id int(11) NOT NULL,
-  page_id int(11) NOT NULL,
-  title varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  data blob NOT NULL,
-  shown datetime DEFAULT NULL,
-  created_at datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  id SERIAL PRIMARY KEY,
+  page_id INTEGER NOT NULL,
+  title VARCHAR(255) COLLATE "C" NOT NULL,
+  data BYTEA NOT NULL,
+  shown TIMESTAMP DEFAULT NULL,
+  created_at TIMESTAMP NOT NULL,
+  username VARCHAR(255) NULL
+);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table CIForms_submissions
---
-ALTER TABLE ciforms_submissions
-  ADD PRIMARY KEY (id);
-
-ALTER TABLE ciforms_submissions ADD username VARCHAR(255) NULL AFTER page_id;
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table CIForms_submissions
---
-ALTER TABLE ciforms_submissions
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
