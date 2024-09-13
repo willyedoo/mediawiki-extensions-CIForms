ef4c56a77a0ddcbd6a4ef936a84452fa5791a930
wisemoon <willyedoo@gmail.com>
2024-09-13 13:38:29 +0200

    Update CIForms_submissions_groups.sql

--- postgres/CIForms_submissions_groups.sql
+++ postgres/CIForms_submissions_groups.sql
@@ -3,11 +3,11 @@
 --
 
 CREATE TABLE IF NOT EXISTS CIForms_submissions_groups (
-  id int(11) NOT NULL,
-  submission_id int(11) NOT NULL,
-  usergroup varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  created_at datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  id SERIAL PRIMARY KEY,
+  submission_id INTEGER NOT NULL,
+  usergroup VARCHAR(255) COLLATE "C" NOT NULL,
+  created_at TIMESTAMP NOT NULL
+);
 
 --
 -- Indexes for dumped tables
@@ -16,15 +16,3 @@ CREATE TABLE IF NOT EXISTS CIForms_submissions_groups (
 --
 -- Indexes for table CIForms_submissions_groups
 --
-ALTER TABLE CIForms_submissions_groups
-  ADD PRIMARY KEY (id);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table CIForms_submissions_groups
---
-ALTER TABLE CIForms_submissions_groups
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
