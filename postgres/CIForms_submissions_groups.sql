a651af687cbfedcb6fd5075babd2acc3e79fc2f1
wisemoon <willyedoo@gmail.com>
2024-09-30 15:55:51 +0200

    Update CIForms_submissions_groups.sql

--- postgres/CIForms_submissions_groups.sql
+++ postgres/CIForms_submissions_groups.sql
@@ -2,10 +2,12 @@
 -- Table structure for table CIForms_submissions_groups
 --
 
-CREATE TABLE IF NOT EXISTS CIForms_submissions_groups (
+CREATE TABLE IF NOT EXISTS ciforms_submissions (
   id SERIAL PRIMARY KEY,
-  submission_id INTEGER NOT NULL,
-  usergroup VARCHAR(255) COLLATE "C" NOT NULL,
+  page_id INTEGER NOT NULL,
+  title VARCHAR(255) COLLATE "C" NOT NULL,
+  data BYTEA NOT NULL,
+  shown TIMESTAMP DEFAULT NULL,
   created_at TIMESTAMP NOT NULL
 );
 --
