a41cb5b22dac700e520481d0db50aa8d8dd0cf4e
wisemoon <willyedoo@gmail.com>
2024-10-01 16:23:38 +0200

    Update CIForms_submissions.sql

--- postgres/CIForms_submissions.sql
+++ postgres/CIForms_submissions.sql
@@ -1,5 +1,4 @@
---
--- Table structure for table CIForms_submissions
+-- Table structure for table `CIForms_submissions`
 --
 
 CREATE TABLE IF NOT EXISTS ciforms_submissions (
@@ -8,26 +7,11 @@ CREATE TABLE IF NOT EXISTS ciforms_submissions (
   title VARCHAR(255) COLLATE "C" NOT NULL,
   data BYTEA NOT NULL,
   shown TIMESTAMP DEFAULT NULL,
-  created_at TIMESTAMP NOT NULL,
-  username VARCHAR(255) NULL
+  created_at TIMESTAMP NOT NULL
 );
 
---
--- Indexes for dumped tables
---
--- Add primary key
-ALTER TABLE ciforms_submissions
-  ADD PRIMARY KEY (id);
-
--- Add new column
+-- Adding a new column `username`
 ALTER TABLE ciforms_submissions
   ADD COLUMN username VARCHAR(255) NULL;
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `CIForms_submissions`
---
-ALTER TABLE ciforms_submissions
-  ALTER COLUMN id SET DEFAULT nextval('ciforms_submissions_id_seq');
+-- Note: SERIAL in PostgreSQL automatically creates a sequence and sets the column as auto-increment.
