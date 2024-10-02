ec2d6e1b2002a07c5e70154fffe77ded8bcabea2
wisemoon <willyedoo@gmail.com>
2024-10-02 15:32:33 +0200

    Update CIForms_submissions_username.sql

--- postgres/CIForms_submissions_username.sql
+++ postgres/CIForms_submissions_username.sql
@@ -1,2 +1,8 @@
-ALTER TABLE CIForms_submissions
-ADD COLUMN username VARCHAR(255) NULL;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns
+                   WHERE table_name='ciforms_submissions' AND column_name='username') THEN
+        ALTER TABLE ciforms_submissions
+        ADD COLUMN username VARCHAR(255) NULL;
+    END IF;
+END $$;
