1f3d5ae4fa440926eac1156f2e1698d51ef05936
wisemoon <willyedoo@gmail.com>
2024-10-03 13:12:59 +0200

    Update CIForms_submissions.sql

--- postgres/CIForms_submissions.sql
+++ postgres/CIForms_submissions.sql
@@ -1,14 +1,14 @@
--- Table structure for table "CIForms_submissions"
+-- Table structure for table `CIForms_submissions`
 
 CREATE TABLE IF NOT EXISTS CIForms_submissions (
   id SERIAL PRIMARY KEY,
   page_id INTEGER NOT NULL,
-  title VARCHAR(255) COLLATE "C" NOT NULL,
+  title VARCHAR(255) NOT NULL,
   data BYTEA NOT NULL,
   shown TIMESTAMP DEFAULT NULL,
   created_at TIMESTAMP NOT NULL,
   username VARCHAR(255) NULL
 );
 
--- Indexes for table "CIForms_submissions"
--- (Primary key is already created with SERIAL type)
+-- Indexes for table `CIForms_submissions`
+-- (Primary key is already defined with SERIAL)
