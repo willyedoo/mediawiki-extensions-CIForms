3fe98d399b78b6b8c64f7fe85b168711a1ccf64a
wisemoon <willyedoo@gmail.com>
2024-10-03 14:14:13 +0200

    Update CIForms_submissions_username.sql

--- postgres/CIForms_submissions_username.sql
+++ postgres/CIForms_submissions_username.sql
@@ -1,2 +1,7 @@
-ALTER TABLE CIForms_submissions
-  ADD COLUMN username VARCHAR(255) NULL;
+ALTER TABLE "CIForms_submissions"
+ADD COLUMN "username" VARCHAR(255) NULL;
+
+ALTER TABLE "CIForms_submissions"
+ADD CONSTRAINT fk_page
+FOREIGN KEY ("page_id")
+REFERENCES "Pages" ("page_id");
