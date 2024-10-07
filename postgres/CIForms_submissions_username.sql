8ad229227e82b32b9fa38cc8fe81dc5289b266e1
wisemoon <willyedoo@gmail.com>
2024-10-07 11:17:46 +0200

    Update CIForms_submissions_username.sql

--- postgres/CIForms_submissions_username.sql
+++ postgres/CIForms_submissions_username.sql
@@ -1,6 +1,1 @@
-ALTER TABLE CIForms_submissions
-    ADD COLUMN username VARCHAR(255);
-
-ALTER TABLE CIForms_submissions
-    SET TABLESPACE page_id_ts
-    FOR COLUMN page_id;
+ALTER TABLE CIForms_submissions ADD COLUMN username VARCHAR(255) NULL;
